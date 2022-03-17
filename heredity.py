Fix: calculation not matching spec

heredity.py
@@ -167,7 +167,6 @@ def ancestors(people, person, ancestor_dict):
 
 
 def ancestors_calc(people, person, one_gene, two_genes):
-    print('ancestors_calc')
     p_gene = 0
     mother = people[person]["mother"]
     father = people[person]["father"]
@@ -364,23 +363,19 @@ def normalize(probabilities):
     print(f'probabilities before normalization: {probabilities}')
 
     # get probabilites of gene and trait as lists for each person
-    persons_list = probabilities.keys()
-    for person in persons_list:
-        print(f'gene content for {person}: {probabilities[person]["gene"]}')
+    probabilities_copy = copy.deepcopy(probabilities)
+    for person in probabilities_copy:
+        print(f'gene content for {person}: {probabilities[person]["gene"]}')            
         gene_list = []
-        gene_list_keys = []
         trait_list = []
-        print(f'gene: {probabilities[person]["gene"]}')
-        for key, value in probabilities[person]["gene"].items():
-            gene_list_keys.append(int(key))
-            gene_list.append(value)
 
-        # append values to trait_list
-        trait_list.append(probabilities[person]["trait"][True])
-        trait_list.append(probabilities[person]["trait"][False])
+        for i in range(3):
+            gene_list.append(probabilities_copy[person]["gene"][i])
+
+        trait_list.append(probabilities_copy[person]["trait"][True])
+        trait_list.append(probabilities_copy[person]["trait"][False])
 
         print(f'gene_list: {gene_list}')
-        print(f'key_list: {gene_list_keys}')
         print(f'trait_list: {trait_list}')
 
         # turn lists into numpy arrays
@@ -388,9 +383,6 @@ def normalize(probabilities):
         trait_arr = np.asarray(trait_list)
 
         # normalize arrays and turn to lists
-        # gene_list_normalized = normalize_arr(gene_arr, 0, 1)
-        # trait_list_normalized = normalize_arr(trait_arr, 0, 1)
-
         gene_arr = gene_arr / gene_arr.sum()
         trait_arr = trait_arr / trait_arr.sum()
 
@@ -403,9 +395,9 @@ def normalize(probabilities):
         print(f'trait_list normalized for {person}: {trait_list}')
 
         # update probabilites with normalized values
-        probabilities[person]["gene"][gene_list_keys[0]] = gene_list[gene_list_keys[0]]
-        probabilities[person]["gene"][gene_list_keys[1]] = gene_list[gene_list_keys[1]]
-        probabilities[person]["gene"][gene_list_keys[2]] = gene_list[gene_list_keys[2]]
+        for i in range(3):
+            probabilities[person]["gene"][i] = gene_list[i]
+
         probabilities[person]["trait"][True] = trait_list[0]
         probabilities[person]["trait"][False] = trait_list[1]
 